7108c748e297bdd471e36394f8ff20f12423fefd
hayatullah farahi <hayatullah.farahi@gmail.com>
2020-04-03 01:19:50 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+
+
+app.get('/', (req, res) => {
+    res.send("Node Server is running. Yay!!")
+})
+
+//Socket Logic
+const socketio = require('socket.io')(http)
+
+socketio.on("connection", (userSocket) => {
+    userSocket.on("send_message", (data) => {
+        userSocket.broadcast.emit("receive_message", data)
+    })
+})
+const port = process.env.PORT
+http.listen(port, ()=>{
+    console.log(`server running on port ${port}`)
+})
