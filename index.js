1259f738a4e6462569d058068a899b8d1768eb25
hayatullah farahi <hayatullah.farahi@gmail.com>
2020-04-04 10:05:14 +0530

    testing sockets

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ socketio.on("connection", (userSocket) => {
     console.log("new connection")
     userSocket.on("send_message", (data) => {
         userSocket.broadcast.emit("receive_message", data.message)
-        console.log(data.body);
+        console.log(data);
     })
 })
 const port = process.env.PORT || 5000;
