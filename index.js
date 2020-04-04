fedd007d29b5e2004d8505f3affe7ddf6f17d01a
hayatullah farahi <hayatullah.farahi@gmail.com>
2020-04-04 09:12:16 +0530

    json received from client

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const app = require('express')()
 const http = require('http').createServer(app)
+const https = require('https')
+const fs = require('fs')
+
+const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+}
 
 
 app.get('/', (req, res) => {
@@ -10,11 +17,15 @@ app.get('/', (req, res) => {
 const socketio = require('socket.io')(http)
 
 socketio.on("connection", (userSocket) => {
+    console.log("new connection")
     userSocket.on("send_message", (data) => {
         userSocket.broadcast.emit("receive_message", data)
+        console.log('data');
     })
 })
-const port = process.env.PORT
-http.listen(port, ()=>{
+const port = process.env.PORT || 5000;
+
+http
+.listen(port, ()=>{
     console.log(`server running on port ${port}`)
 })
