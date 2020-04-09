a10463fbedf58ea8eecfe24cab8ac136e4b55dd6
hayatullah farahi <hayatullah.farahi@gmail.com>
2020-04-09 10:42:26 +0530

    original

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 const app = require('express')()
 const http = require('http').createServer(app)
-const https = require('https')
-const fs = require('fs')
-
-const options = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
-}
 
 
 app.get('/', (req, res) => {
@@ -17,17 +10,47 @@ app.get('/', (req, res) => {
 const socketio = require('socket.io')(http)
 
 socketio.on("connection", (userSocket) => {
-    console.log("new connection")
     userSocket.on("send_message", (data) => {
         userSocket.broadcast.emit("receive_message", data)
-        console.log(data);
-        socketio.emit("receive_request", data)
-        console.log(data);
     })
 })
-const port = process.env.PORT || 5000;
+
+http.listen(process.env.PORT)
 
 
-http.listen(port, ()=>{
-    console.log(`server running on port ${port}`)
-})
+
+
+
+// const app = require('express')()
+// const http = require('http').createServer(app)
+// const https = require('https')
+// const fs = require('fs')
+
+// const options = {
+//     key: fs.readFileSync('key.pem'),
+//     cert: fs.readFileSync('cert.pem')
+// }
+
+
+// app.get('/', (req, res) => {
+//     res.send("Node Server is running. Yay!!")
+// })
+
+// //Socket Logic
+// const socketio = require('socket.io')(http)
+
+// socketio.on("connection", (userSocket) => {
+//     console.log("new connection")
+//     userSocket.on("send_message", (data) => {
+//         userSocket.broadcast.emit("receive_message", data)
+//         console.log(data);
+//         socketio.emit("receive_request", data)
+//         console.log(data);
+//     })
+// })
+// const port = process.env.PORT || 5000;
+
+
+// http.listen(port, ()=>{
+//     console.log(`server running on port ${port}`)
+// })
