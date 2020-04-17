a5a072678915fbacd4c96d8fa3e80cadb7aefd12
hayatullah farahi <hayatullah.farahi@gmail.com>
2020-04-18 01:37:41 +0530

    send location socket added

--- index.js
+++ index.js
@@ -8,32 +8,37 @@ const msg = []
 const requests = []
 
 
-// wsServer = new WebSocketServer({
-//     httpServer: http,
-//     // You should not use autoAcceptConnections for production
-//     // applications, as it defeats all standard cross-origin protection
-//     // facilities built into the protocol and the browser.  You should
-//     // *always* verify the connection's origin and decide whether or not
-//     // to accept it.
-//                 // autoAcceptConnections: false
-// })
+wsServer = new WebSocketServer({
+    httpServer: http,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+                // autoAcceptConnections: false
+})
 
-// wsServer.on('request', function(request) {
-//     const connection = request.accept(null, request.origin)
-//     connection.on('message', function(message) {
-//         if(message.type === 'utf8'){
-//             console.log("Recived message:" + message.utf8Data)
-//             msg.push(`${message.utf8Data}`)
-//             console.log(msg)
-//             connection.send(message.utf8Data)
-//         }
-//     })
-//     connection.on('close',  function(reasonCode, description){
-//         // console.log((new Date())  + 'peer' + connection.remoteAddress + ' disconnected')
-//     })
-
-
-// })
+wsServer.on('request', function(request) {
+    const connection = request.accept(null, request.origin)
+    console.log("New WebSocket Connection")
+    connection.on('message', function(message) {
+        if(message.type === 'utf8'){
+            console.log("Recived message:       " + message.utf8Data)
+            // msg.push(`${message.utf8Data}`)
+            // console.log(msg)
+            // connection.send(message.utf8Data)
+            // const data = {
+            //     "name": message.utf8Data,
+            //     "price": 345
+            // }
+            connection.send(message.utf8Data)
+            wsServer.broadcast(message.utf8Data)
+        }
+    })
+    connection.on('close',  function(reasonCode, description){
+        // console.log((new Date())  + 'peer' + connection.remoteAddress + ' disconnected')
+    })
+})
 
 const options = {
     key: fs.readFileSync('key.pem'),
@@ -50,6 +55,7 @@ const socketio = require('socket.io')(http)
 
 socketio.on("connection", (userSocket) => {
     console.log("new connection")
+
     userSocket.on("send_message", (data) => {
         requests.push(data)
         userSocket.broadcast.emit("receive_message", data)
@@ -57,8 +63,13 @@ socketio.on("connection", (userSocket) => {
         socketio.emit("receive_request", data)
         console.log(data);
     })
+
+    userSokcet.on("send_location", (data)=>{
+        userSocket.broadcast.emit("receive_location", data)
+    })
 })
 const port = process.env.PORT || 5000;
+// const port =  5000;
 
 
 http.listen(port, ()=>{
